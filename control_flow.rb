e8ad0c8345ab83995f8776c3fd3d2ef78ab97e5d
brigitteorina <brigitteorina@gmail.com>
2023-05-20 11:04:28 +0300

    complete

--- control_flow.rb
+++ control_flow.rb
@@ -1,22 +1,5 @@
 def admin_login(username, password)
-  # your code here
-end
-
-def hows_the_weather(temperature)
-  # your code here
-end
-
-def fizzbuzz(num)
-  # your code here
-end
-
-def calculator(operation, num1, num2)
-  # your code here
-end
-
-
-def admin_login(username, password)
-  if (username == "admin" || username == "ADMIN") && password == "12345"
+  if (username.downcase == "admin" || username.upcase == "ADMIN") && password == "12345"
     "Access granted"
   else
     "Access denied"
@@ -58,34 +41,68 @@ def calculator(operation, num1, num2)
   when "/"
     num1 / num2
   else
-    "Invalid operation!"
+    puts "Invalid operation!"
+    nil
   end
 end
-#calling the methods
+
+
+# Calling admin_login method
 puts admin_login("sudo", "12345")
 # Output: Access denied
 
 puts admin_login("admin", "12345")
 # Output: Access granted
 
+puts admin_login("ADMIN", "12345")
+# Output: Access granted
+
+
+# Calling hows_the_weather method
 puts hows_the_weather(33)
 # Output: It's brisk out there!
 
 puts hows_the_weather(99)
 # Output: It's too dang hot out there!
 
+puts hows_the_weather(75)
+# Output: It's perfect out there!
+
+
+# Calling fizzbuzz method
+puts fizzbuzz(1)
+# Output: 1
+
+puts fizzbuzz(2)
+# Output: 2
+
 puts fizzbuzz(3)
 # Output: Fizz
+
+puts fizzbuzz(4)
+# Output: 4
 
 puts fizzbuzz(5)
 # Output: Buzz
 
+puts fizzbuzz(15)
+# Output: FizzBuzz
+
+
+# Calling calculator method
 puts calculator("+", 1, 1)
 # Output: 2
+
+puts calculator("-", 3, 1)
+# Output: 2
+
+puts calculator("*", 3, 2)
+# Output: 6
 
 puts calculator("/", 4, 2)
 # Output: 2
 
 puts calculator("nope", 4, 2)
 # Output: Invalid operation!
+# Output: nil
 
